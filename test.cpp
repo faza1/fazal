458952f5bd93d440587bc3c45fcd44e1dc829c45
faza1 <fazalraby8@gmail.com>
2017-02-18 14:51:31 +0600

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+
+int main ()
+
+{
+    int num_1 ;
+    num_1= 13;
+    float num_2;
+    num_2= 2.09;
+    cout <<num_1<<endl;
+    cout <<num_2<<endl;
+
+   return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
