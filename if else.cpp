532e4c073c65ca5904372585622e9d87f82181b5
faza1 <fazalraby8@gmail.com>
2017-02-23 15:08:18 +0600

    Add files via upload

---
+++ if else.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+using namespace std;
+ int main(){
+
+  cout<<"please a enter a number:";
+  cin>>number;
+  if(number>0);
+  {
+      cout<<number is positive<<endl;
+  }
+
+else(number<0);
+{
+    cout<<number is negative<<endl;
+}
+
+
+
+ return 0;
+}
