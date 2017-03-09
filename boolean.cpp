a67d1637191794923b82cfd318e62f1842ac91cc
faza1 <fazalraby8@gmail.com>
2017-03-09 16:08:33 +0600

    Add files via upload

---
+++ boolean.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+    int main() {
+
+  cout <<" 23 > 3 ="<< (23>3)<< endl;
+  cout <<"12 > 123="<<(12>123)<<endl;
+  cout <<"4 == 4=" <<( 4==4 )  <<endl;
+  cout <<"4 <= 4 ="<<(4<=4)<<endl;
+  cout <<"4 >=4="<< (4>=4)<<endl;
+
+
+
+
+return 0;
+}
